3d3777e15b5aef02dbf7ca93c7176963544740c1
小鬼 <771267382@qq.com>
2016-05-25 15:56:45 +0800

    改动了ViewController，新添加了一个FirstViewController

---
+++ 仓库测试/仓库测试/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  仓库测试
+//
+//  Created by 洪铭翔 on 16/5/25.
+//  Copyright © 2016年 洪铭翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
--- 仓库测试/仓库测试/ViewController.m
+++ 仓库测试/仓库测试/ViewController.m
@@ -16,7 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
+    label.text = @"NSString";
+    label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
